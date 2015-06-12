72089c3de8a135f4307a862bbba70fe8fe936afd
Brandon Mulcahy <brandon@jangler.info>
2015-06-12 18:38:40 -0400

    Add and test all functions for 0.1.0

--- readlike.py
+++ readlike.py
@@ -1,44 +1,108 @@
-"""This module provides access to GNU Readline-like line editing functions."""
+"""
+This module provides access to GNU Readline-like line editing functions.
+"""
 
 __all__ = ['edit', 'keys']
 
 
 def _backward_char(text, pos):
-    # move back a character.
+    # Move pos back a character.
     return text, max(0, pos - 1)
 
 
 def _backward_delete_char(text, pos):
-    # delete the character behind the cursor.
+    # Delete the character behind pos.
     if pos == 0:
         return text, pos
     return text[:pos - 1] + text[pos:], pos - 1
 
 
+def _backward_kill_word(text, pos):
+    # Kill the word behind pos. Word boundaries are the same as those
+    # used by _backward_word.
+    text, new_pos = _backward_word(text, pos)
+    return text[:new_pos] + text[pos:], new_pos
+
+
+def _backward_word(text, pos):
+    # Move pos back to the start of the current or previous word. Words
+    # are composed of alphanumeric characters (letters and digits).
+    while pos > 0 and not text[pos - 1].isalnum():
+        pos -= 1
+    while pos > 0 and text[pos - 1].isalnum():
+        pos -= 1
+    return text, pos
+
+
 def _beginning_of_line(text, pos):
-    # move to the start of the current line.
+    # Move pos to the start of text.
     return text, 0
 
 
+def _capitalize_word(text, pos):
+    # Capitalize the current (or following) word.
+    while pos < len(text) and not text[pos].isalnum():
+        pos += 1
+    if pos < len(text):
+        text = text[:pos] + text[pos].upper() + text[pos + 1:]
+    while pos < len(text) and text[pos].isalnum():
+        pos += 1
+    return text, pos
+
+
 def _delete_char(text, pos):
-    # delete the character at pos.
+    # Delete the character at pos.
     return text[:pos] + text[pos + 1:], pos
 
 
+def _delete_horizontal_space(text, pos):
+    # Delete all spaces and tabs around pos.
+    while pos > 0 and text[pos - 1].isspace():
+        pos -= 1
+    end_pos = pos
+    while end_pos < len(text) and text[end_pos].isspace():
+        end_pos += 1
+    return text[:pos] + text[end_pos:], pos
+
+
+def _downcase_word(text, pos):
+    # Lowercase the current (or following) word.
+    text, new_pos = _forward_word(text, pos)
+    return text[:pos] + text[pos:new_pos].lower() + text[new_pos:], new_pos
+
+
 def _forward_char(text, pos):
-    # move forward a character.
+    # Move pos forward a character.
     return text, min(pos + 1, len(text))
 
 
+def _forward_word(text, pos):
+    # Move pos forward to the end of the next word. Words are composed
+    # of alphanumeric characters (letters and digits).
+    while pos < len(text) and not text[pos].isalnum():
+        pos += 1
+    while pos < len(text) and text[pos].isalnum():
+        pos += 1
+    return text, pos
+
+
 def _kill_line(text, pos):
-    # kill the text from pos to the end of the line.
+    # Kill from pos to the end of text.
     return text[:pos], pos
 
 
+def _kill_word(text, pos):
+    # Kill from pos to the end of the current word, or if between words,
+    # to the end of the next word. Word boundaries are the same as those
+    # used by _forward_word.
+    text, end_pos = _forward_word(text, pos)
+    return text[:pos] + text[end_pos:], pos
+
+
 def _transpose_chars(text, pos):
-    # drag the character before pos forward over the character at pos, moving
-    # pos forward as well. if point is at the end of the line, then this
-    # transposes the two characters before point.
+    # Drag the character before pos forward over the character at pos,
+    # moving pos forward as well. If pos is at the end of text, then
+    # this transposes the two characters before pos.
     if len(text) < 2 or pos == 0:
         return text, pos
     if pos == len(text):
@@ -46,13 +110,27 @@ def _transpose_chars(text, pos):
     return text[:pos - 1] + text[pos] + text[pos - 1] + text[pos + 1:], pos + 1
 
 
+def _transpose_words(text, pos):
+    # Drag the word before pos past the word after pos, moving pos over
+    # that word as well. If pos is at the end of text, this transposes
+    # the last two words in text.
+    text, end2 = _forward_word(text, pos)
+    text, start2 = _backward_word(text, end2)
+    text, start1 = _backward_word(text, start2)
+    text, end1 = _forward_word(text, start1)
+    if start1 == start2:
+        return text, pos
+    return text[:start1] + text[start2:end2] + text[end1:start2:] + \
+        text[start1:end1] + text[end2:], end2
+
+
 def _unix_line_discard(text, pos):
-    # kill backward from pos to the beginning of the line.
+    # Kill backward from pos to the beginning of text.
     return text[pos:], 0
 
 
 def _unix_word_rubout(text, pos):
-    # kill the word behind pos, using white space as a word boundary.
+    # Kill the word behind pos, using white space as a word boundary.
     words = text[:pos].rsplit(None, 1)
     if len(words) < 2:
         return text[pos:], 0
@@ -61,32 +139,46 @@ def _unix_word_rubout(text, pos):
         return text[:index] + text[pos:], index
 
 
+def _upcase_word(text, pos):
+    # Uppercase the current (or following) word.
+    text, new_pos = _forward_word(text, pos)
+    return text[:pos] + text[pos:new_pos].upper() + text[new_pos:], new_pos
+
+
 _key_bindings = {
-    # not implemented: C-@ (set-mark)
     'ctrl a': _beginning_of_line,
     'ctrl b': _backward_char,
     'ctrl d': _delete_char,
     'ctrl f': _forward_char,
-    # not implemented: C-G (abort)
     'ctrl h': _backward_delete_char,
-    # not implemented: C-I (complete)
-    # not implemented: C-J (accept-line)
+    'backspace': _backward_delete_char,
+    # TODO: C-I/tab (complete) - 0.2.0
     'ctrl k': _kill_line,
-    # not implemented: C-L (clear-screen)
-    # not implemented: C-M (accept-line)
-    # not implemented: C-N (next-history)
-    # not implemented: C-P (previous-history)
-    # not implemented: C-Q (quoted-insert)
-    # not implemented: C-R (reverse-search-history)
-    # not implemented: C-S (forward-search-history)
+    # TODO: C-N (next-history) - 0.2.0
+    # TODO: C-P (previous-history) - 0.2.0
     'ctrl t': _transpose_chars,
     'ctrl u': _unix_line_discard,
-    # not implemented: C-V (quoted-insert)
     'ctrl w': _unix_word_rubout,
-    # not implemented: C-Y (yank)
-    # not implemented: C-] (character-search)
-    # not implemented: C-_ (undo)?
-    # not implemented: C-? (backward-delete-char)
+    # TODO: C-Y (yank) - 0.3.0
+    'ctrl meta h': _backward_kill_word,
+    'meta backspace': _backward_kill_word,
+    # TODO: M-C-I (tab-insert) - 0.2.0
+    # TODO: M-C-Y (yank-nth-arg) - 0.3.0
+    # TODO: M-C-[ (complete) - 0.2.0
+    # TODO: M-* (insert-completions) - 0.2.0
+    # TODO: M-. (yank-last-arg) - 0.3.0
+    # TODO: M-< (beginning-of-history) - 0.2.0
+    # TODO: M-> (end-of-history) - 0.2.0
+    'meta b': _backward_word,
+    'meta c': _capitalize_word,
+    'meta d': _kill_word,
+    'meta f': _forward_word,
+    'meta l': _downcase_word,
+    'meta t': _transpose_words,
+    'meta u': _upcase_word,
+    # TODO: M-Y (yank-pop) - 0.3.0
+    'meta \\': _delete_horizontal_space,
+    # TODO: M-_ (yank-last-arg) - 0.3.0
 }
 
 _keys = frozenset(_key_bindings.keys())
--- tests/test_readlike.py
+++ tests/test_readlike.py
@@ -16,20 +16,66 @@ class TestReadlike(unittest.TestCase):
         self.assertEqual(rl._backward_delete_char('test', 1), ('est', 0))
         self.assertEqual(rl._backward_delete_char('test', 4), ('tes', 3))
 
+    def test_backward_kill_word(self):
+        self.assertEqual(rl._backward_kill_word('', 0), ('', 0))
+        self.assertEqual(rl._backward_kill_word('test', 0), ('test', 0))
+        self.assertEqual(rl._backward_kill_word('test', 2), ('st', 0))
+        self.assertEqual(rl._backward_kill_word('hi. yo. ', 4), ('yo. ', 0))
+        self.assertEqual(rl._backward_kill_word('hi. yo. ', 5), ('hi. o. ', 4))
+        self.assertEqual(rl._backward_kill_word('hi. yo. ', 8), ('hi. ', 4))
+
+    def test_backward_word(self):
+        self.assertEqual(rl._backward_word('', 0), ('', 0))
+        self.assertEqual(rl._backward_word('test', 0), ('test', 0))
+        self.assertEqual(rl._backward_word('hi. yo. ', 3), ('hi. yo. ', 0))
+        self.assertEqual(rl._backward_word('hi. yo. ', 4), ('hi. yo. ', 0))
+        self.assertEqual(rl._backward_word('hi. yo. ', 5), ('hi. yo. ', 4))
+        self.assertEqual(rl._backward_word('hi. yo. ', 8), ('hi. yo. ', 4))
+
     def test_beginning_of_line(self):
         self.assertEqual(rl._beginning_of_line('', 0), ('', 0))
         self.assertEqual(rl._beginning_of_line('test', 0), ('test', 0))
         self.assertEqual(rl._beginning_of_line('test', 4), ('test', 0))
+
+    def test_capitalize_word(self):
+        self.assertEqual(rl._capitalize_word('', 0), ('', 0))
+        self.assertEqual(rl._capitalize_word('test', 0), ('Test', 4))
+        self.assertEqual(rl._capitalize_word('test', 3), ('tesT', 4))
+        self.assertEqual(rl._capitalize_word('test', 4), ('test', 4))
+        self.assertEqual(rl._capitalize_word('. test', 0), ('. Test', 6))
+        self.assertEqual(rl._capitalize_word('test  ', 1), ('tEst  ', 4))
 
     def test_delete_char(self):
         self.assertEqual(rl._delete_char('', 0), ('', 0))
         self.assertEqual(rl._delete_char('test', 0), ('est', 0))
         self.assertEqual(rl._delete_char('test', 4), ('test', 4))
 
+    def test_delete_horizontal_space(self):
+        self.assertEqual(rl._delete_horizontal_space('', 0), ('', 0))
+        self.assertEqual(rl._delete_horizontal_space('hi', 1), ('hi', 1))
+        self.assertEqual(rl._delete_horizontal_space(' hi ', 2), (' hi ', 2))
+        self.assertEqual(rl._delete_horizontal_space('\t \t', 1), ('', 0))
+        self.assertEqual(rl._delete_horizontal_space('.  .', 2), ('..', 1))
+
+    def test_downcase_word(self):
+        self.assertEqual(rl._downcase_word('', 0), ('', 0))
+        self.assertEqual(rl._downcase_word('TEST', 0), ('test', 4))
+        self.assertEqual(rl._downcase_word('TEST', 2), ('TEst', 4))
+        self.assertEqual(rl._downcase_word('TEST', 4), ('TEST', 4))
+        self.assertEqual(rl._downcase_word('. TEST', 0), ('. test', 6))
+        self.assertEqual(rl._downcase_word('TEST  ', 1), ('Test  ', 4))
+
     def test_forward_char(self):
         self.assertEqual(rl._forward_char('', 0), ('', 0))
         self.assertEqual(rl._forward_char('test', 0), ('test', 1))
         self.assertEqual(rl._forward_char('test', 4), ('test', 4))
+
+    def test_forward_word(self):
+        self.assertEqual(rl._forward_word('', 0), ('', 0))
+        self.assertEqual(rl._forward_word('test', 4), ('test', 4))
+        self.assertEqual(rl._forward_word('hi yo ', 1), ('hi yo ', 2))
+        self.assertEqual(rl._forward_word('hi yo ', 3), ('hi yo ', 5))
+        self.assertEqual(rl._forward_word('hi yo ', 5), ('hi yo ', 6))
 
     def test_kill_line(self):
         self.assertEqual(rl._kill_line('', 0), ('', 0))
@@ -37,11 +83,28 @@ class TestReadlike(unittest.TestCase):
         self.assertEqual(rl._kill_line('test', 2), ('te', 2))
         self.assertEqual(rl._kill_line('test', 4), ('test', 4))
 
+    def test_kill_word(self):
+        self.assertEqual(rl._kill_word('', 0), ('', 0))
+        self.assertEqual(rl._kill_word('test', 4), ('test', 4))
+        self.assertEqual(rl._kill_word('hi yo ', 1), ('h yo ', 1))
+        self.assertEqual(rl._kill_word('hi yo ', 3), ('hi  ', 3))
+        self.assertEqual(rl._kill_word('hi yo ', 5), ('hi yo', 5))
+
     def test_transpose_chars(self):
         self.assertEqual(rl._transpose_chars('', 0), ('', 0))
         self.assertEqual(rl._transpose_chars('test', 0), ('test', 0))
         self.assertEqual(rl._transpose_chars('test', 2), ('tset', 3))
         self.assertEqual(rl._transpose_chars('test', 4), ('tets', 4))
+
+    def test_transpose_words(self):
+        self.assertEqual(rl._transpose_words('', 0), ('', 0))
+        self.assertEqual(rl._transpose_words('test', 2), ('test', 2))
+        self.assertEqual(rl._transpose_words('ab cd ef', 1), ('ab cd ef', 1))
+        self.assertEqual(rl._transpose_words('ab cd ef', 2), ('cd ab ef', 5))
+        self.assertEqual(rl._transpose_words('ab cd ef', 4), ('cd ab ef', 5))
+        self.assertEqual(rl._transpose_words('ab cd ef', 5), ('ab ef cd', 8))
+        self.assertEqual(rl._transpose_words('ab cd ef', 8), ('ab ef cd', 8))
+        self.assertEqual(rl._transpose_words('ab,./cd', 2), ('cd,./ab', 7))
 
     def test_unix_line_discard(self):
         self.assertEqual(rl._unix_line_discard('', 0), ('', 0))
@@ -56,6 +119,14 @@ class TestReadlike(unittest.TestCase):
         self.assertEqual(rl._unix_word_rubout('hi  yo  ', 4), ('yo  ', 0))
         self.assertEqual(rl._unix_word_rubout('hi  yo  ', 5), ('hi  o  ', 4))
         self.assertEqual(rl._unix_word_rubout('hi  yo  ', 8), ('hi  ', 4))
+
+    def test_upcase_word(self):
+        self.assertEqual(rl._upcase_word('', 0), ('', 0))
+        self.assertEqual(rl._upcase_word('test', 0), ('TEST', 4))
+        self.assertEqual(rl._upcase_word('test', 2), ('teST', 4))
+        self.assertEqual(rl._upcase_word('test', 4), ('test', 4))
+        self.assertEqual(rl._upcase_word('. test', 0), ('. TEST', 6))
+        self.assertEqual(rl._upcase_word('test  ', 1), ('tEST  ', 4))
 
     def test_edit(self):
         self.assertEqual(rl.edit('test', 4, 's'), ('tests', 5))
